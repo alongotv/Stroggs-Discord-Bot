ab12bd0809bd9a90756854c456b64198142ea3ae
Vladimir Vetrov <pdastore1@gmail.com>
2022-08-05 22:06:50 +0300

    Add class MessageCreateEventTransmitter

---
+++ src/main/kotlin/data/MessageCreateEventTransmitter.kt
@@ -0,0 +1,15 @@
+package data
+
+import dev.kord.core.event.message.MessageCreateEvent
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+
+class MessageCreateEventTransmitter {
+    private val _messagesFlow = MutableSharedFlow<MessageCreateEvent>()
+    val messagesFlow: SharedFlow<MessageCreateEvent> = _messagesFlow
+
+    suspend fun handle(event: MessageCreateEvent, ignoresBots: Boolean = true) {
+        if (event.message.author?.isBot == true && ignoresBots) return
+        _messagesFlow.emit(event)
+    }
+}
