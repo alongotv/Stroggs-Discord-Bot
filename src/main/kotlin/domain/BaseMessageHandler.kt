74ea9a4a711fb666551f45093b01fc67ff8d676d
Vladimir Vetrov <pdastore1@gmail.com>
2022-08-05 23:14:04 +0300

    Add abstract class BaseMessageHandler

---
+++ src/main/kotlin/domain/BaseMessageHandler.kt
@@ -0,0 +1,18 @@
+package domain
+
+import data.MessageCreateEventTransmitter
+import dev.kord.core.event.message.MessageCreateEvent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filter
+
+abstract class BaseMessageHandler(messageCreateEventTransmitter: MessageCreateEventTransmitter) {
+
+    val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    abstract val predicate: (MessageCreateEvent) -> Boolean
+    val messages: Flow<MessageCreateEvent> by lazy { messageCreateEventTransmitter.messagesFlow.filter(predicate) }
+
+    abstract suspend fun setup()
+}
