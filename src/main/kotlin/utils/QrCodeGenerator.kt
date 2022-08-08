8f09bb0d2b85388c17f1f79a461ef47b4bdb3f84
Vladimir Vetrov <pdastore1@gmail.com>
2022-08-08 17:34:45 +0300

    Fix cyrillic letters getting encoded into '?' symbols

--- src/main/kotlin/utils/QrCodeGenerator.kt
+++ src/main/kotlin/utils/QrCodeGenerator.kt
@@ -1,6 +1,7 @@
 package utils
 
 import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
 import com.google.zxing.MultiFormatWriter
 import com.google.zxing.WriterException
 import com.google.zxing.client.j2se.MatrixToImageWriter
@@ -16,16 +17,15 @@ class QrCodeGenerator {
     /** Generates PNG file containing a QR code from [data] and saves it into [path] */
     suspend fun generateQrCode(data: String, path: String, charset: String = "UTF-8", h: Int, w: Int): String =
         suspendCancellableCoroutine { continuation ->
-            //the BitMatrix class represents the 2D matrix of bits
-            //MultiFormatWriter is a factory class that finds the appropriate Writer subclass for the BarcodeFormat requested and encodes the barcode with the supplied contents.
             try {
+                // Required to support cyrillic alphabet
+                val hints = mapOf(EncodeHintType.CHARACTER_SET to "UTF-8")
+                // Encode data to QR code image 2D bit matrix
                 val matrix = MultiFormatWriter().encode(
-                    String(data.toByteArray(charset(charset))),
-                    BarcodeFormat.QR_CODE,
-                    w,
-                    h
+                    String(data.toByteArray(charset(charset))), BarcodeFormat.QR_CODE, w, h, hints
                 )
-
+                // MultiFormatWriter is a factory class that finds the appropriate Writer subclass for the BarcodeFormat requested and encodes the barcode with the supplied contents.
+                // Create an image file in the provided path using the matrix
                 MatrixToImageWriter.writeToPath(matrix, path.substring(path.lastIndexOf('.') + 1), Path(path))
                 continuation.resume(path)
             } catch (e: WriterException) {
