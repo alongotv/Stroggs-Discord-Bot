00e2c106bb95aee10f4c1d2683b5a2ee24a0d5c3
Vladimir Vetrov <pdastore1@gmail.com>
2022-08-08 16:59:59 +0300

    Add fun deleteFileFromPath

--- src/main/kotlin/utils/FileUtils.kt
+++ src/main/kotlin/utils/FileUtils.kt
@@ -1,7 +1,12 @@
 package utils
 
 import java.io.InputStream
+import java.nio.file.Files
+import kotlin.io.path.Path
 
 object FileUtils {
     fun getResourceAsStream(name: String): InputStream? = object {}::class.java.getResourceAsStream(name)
+    fun deleteFileFromPath(string: String) {
+        Files.delete(Path(string))
+    }
 }
