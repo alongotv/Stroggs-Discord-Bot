773fe2202957698f5b5bd6c7e7fd8565340b88e1
Vladimir Vetrov <pdastore1@gmail.com>
2022-08-08 20:25:49 +0300

    Add dependencies to AppCompositionRoot

--- src/main/kotlin/di/AppCompositionRoot.kt
+++ src/main/kotlin/di/AppCompositionRoot.kt
@@ -2,20 +2,32 @@ package di
 
 import data.MessageCreateEventTransmitter
 import domain.MessageHandlerFactory
+import domain.scenario.GenerateQrCodeFromTextScenario
 import domain.usecase.GenerateQrCodeUseCase
 import domain.usecase.RemoveLocalFileUseCase
+import domain.usecase.ResolveQrCodeUseCase
 import utils.qr.QrCodeGenerator
+import utils.qr.QrCodeResolver
 
 class AppCompositionRoot private constructor() {
 
     val messageCreateEventTransmitter = MessageCreateEventTransmitter()
 
     private val qrCodeGenerator by lazy { QrCodeGenerator() }
+    private val qrCodeResolver by lazy { QrCodeResolver() }
     private val generateQrCodeUseCase by lazy { GenerateQrCodeUseCase(qrCodeGenerator) }
     private val removeLocalFileUseCase by lazy { RemoveLocalFileUseCase() }
+    private val generateQrCodeFromTextScenario by lazy {
+        GenerateQrCodeFromTextScenario(
+            generateQrCodeUseCase,
+            removeLocalFileUseCase
+        )
+    }
+
+    private val resolveQrCodeUseCase by lazy { ResolveQrCodeUseCase(qrCodeResolver) }
 
     val messageHandlerFactory =
-        MessageHandlerFactory(messageCreateEventTransmitter, generateQrCodeUseCase, removeLocalFileUseCase)
+        MessageHandlerFactory(messageCreateEventTransmitter, generateQrCodeFromTextScenario, resolveQrCodeUseCase)
 
     companion object {
         lateinit var self: AppCompositionRoot
