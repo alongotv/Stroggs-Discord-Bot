f6ba4e7eb9a09810fef1b1d4040816b09af63d67
Vladimir Vetrov <pdastore1@gmail.com>
2022-08-08 20:25:34 +0300

    Add suspend fun downloadFile

--- src/main/kotlin/utils/FileUtils.kt
+++ src/main/kotlin/utils/FileUtils.kt
@@ -1,12 +1,26 @@
 package utils
 
+import kotlinx.coroutines.suspendCancellableCoroutine
+import java.io.IOException
 import java.io.InputStream
+import java.net.URL
 import java.nio.file.Files
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 import kotlin.io.path.Path
 
 object FileUtils {
     fun getResourceAsStream(name: String): InputStream? = object {}::class.java.getResourceAsStream(name)
     fun deleteFileFromPath(string: String) {
         Files.delete(Path(string))
+    }
+
+    suspend fun downloadFile(url: URL) = suspendCancellableCoroutine<InputStream> {
+        val urlConnection = url.openConnection()
+        try {
+            it.resume(urlConnection.getInputStream())
+        } catch (e: IOException) {
+            it.resumeWithException(e)
+        }
     }
 }
