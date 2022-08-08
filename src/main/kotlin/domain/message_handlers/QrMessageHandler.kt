a3209ada0be2e90fe84a8d541b73c9819690a212
Vladimir Vetrov <pdastore1@gmail.com>
2022-08-08 20:29:26 +0300

    Update QrMessageHandler

--- src/main/kotlin/domain/message_handlers/QrMessageHandler.kt
+++ src/main/kotlin/domain/message_handlers/QrMessageHandler.kt
@@ -2,48 +2,71 @@ package domain.message_handlers
 
 import data.MessageCreateEventTransmitter
 import dev.kord.core.behavior.channel.createMessage
+import dev.kord.core.entity.Attachment
 import dev.kord.core.event.message.MessageCreateEvent
 import domain.BaseMessageHandler
-import domain.usecase.GenerateQrCodeUseCase
-import domain.usecase.RemoveLocalFileUseCase
+import domain.scenario.GenerateQrCodeFromTextScenario
+import domain.usecase.ResolveQrCodeUseCase
 import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import kotlin.io.path.Path
+import utils.FileUtils
+import java.net.URL
 
 class QrMessageHandler(
     messageCreateEventTransmitter: MessageCreateEventTransmitter,
-    private val generateQrCodeUseCase: GenerateQrCodeUseCase,
-    private val removeLocalQrCodeUseCase: RemoveLocalFileUseCase
+    private val generateQrCodeFromTextScenario: GenerateQrCodeFromTextScenario,
+    private val resolveQrCodeUseCase: ResolveQrCodeUseCase
 ) : BaseMessageHandler(
     messageCreateEventTransmitter
 ) {
     override val predicate: (MessageCreateEvent) -> Boolean
         get() = { event ->
-            event.message.content.startsWith("!qrencode")
+            event.message.content.startsWith("!qrencode") || event.message.content.startsWith("!qrdecode")
         }
 
     override suspend fun setup() {
         scope.launch {
             messages.collect { event ->
+                val channel = event.message.channel
                 val messageContent = event.message.content
 
+                val senderUsername = event.message.author?.mention ?: "User"
+
                 if (messageContent.length > 500) {
-                    event.message.channel.createMessage("The encoded message is too long. Remove unnecessary data.")
+                    channel.createMessage("${senderUsername}, the encoded message is too long. Remove unnecessary data.")
                     return@collect
                 }
 
                 if (messageContent.contains("qrencode")) {
                     withContext(NonCancellable) {
-                        val contentToEncode = messageContent.removePrefix("!qrencode ")
-                        val qrCodePath = "qrcode-${System.nanoTime()}.png"
-
-                        generateQrCodeUseCase(contentToEncode, qrCodePath, 300, 300)
-                        event.message.channel.createMessage("${event.message.author?.username} has generated the following qr code:")
-                        event.message.channel.createMessage {
-                            addFile(Path(qrCodePath))
+                        val contentToEncode = messageContent.removePrefix("!qrencode").trim()
+                        if (contentToEncode.isBlank()) {
+                            channel.createMessage("${senderUsername}, please enter a message for further encoding.")
+                            return@withContext
+                        } else {
+                            generateQrCodeFromTextScenario(contentToEncode, event)
                         }
-                        removeLocalQrCodeUseCase(qrCodePath)
+                    }
+                } else if (messageContent.contains("qrdecode")) {
+                    val messageAttachments = event.message.attachments
+
+                    if (messageAttachments.size != 1 || !messageAttachments.first().isImage) {
+                        channel.createMessage("${senderUsername}, please attach exactly one image file.")
+                        return@collect
+                    }
+
+                    val fileToResolve: Attachment = messageAttachments.first()
+                    val inputStream = FileUtils.downloadFile(URL(fileToResolve.url))
+
+                    try {
+                        val resolvedQrCodeText = resolveQrCodeUseCase(inputStream)
+                        channel.createMessage {
+                            this.content =
+                                "@${senderUsername}, the bot has found \"$resolvedQrCodeText\" encoded in your picture."
+                        }
+                    } catch (e: Exception) {
+                        channel.createMessage("${senderUsername}, the bot was unable to find any QR codes in the provided picture")
                     }
                 }
             }
