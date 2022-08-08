07bba09166efafe35fe8c40790e9b048032c34bd
Vladimir Vetrov <pdastore1@gmail.com>
2022-08-08 15:26:17 +0300

    Move kek images to ../kek_images/ resource folder

--- src/main/kotlin/constants/Constants.kt
+++ src/main/kotlin/constants/Constants.kt
@@ -1,8 +1,8 @@
 package constants
 
-const val KEK_IMAGE_PATH = "/kek_image_1.png"
-const val KEK_IMAGE_PATH_2 = "/kek_image_2.png"
-const val KEK_IMAGE_PATH_3 = "/kek_image_3.png"
-const val KEK_IMAGE_PATH_4 = "/kek_image_4.png"
-const val KEK_IMAGE_PATH_5 = "/kek_image_5.png"
+const val KEK_IMAGE_PATH = "/kek_images/kek_image_1.png"
+const val KEK_IMAGE_PATH_2 = "/kek_images/kek_image_2.png"
+const val KEK_IMAGE_PATH_3 = "/kek_images/kek_image_3.png"
+const val KEK_IMAGE_PATH_4 = "/kek_images/kek_image_4.png"
+const val KEK_IMAGE_PATH_5 = "/kek_images/kek_image_5.png"
 val KEK_IMAGES_PATHS_ARRAY = arrayOf(KEK_IMAGE_PATH, KEK_IMAGE_PATH_2, KEK_IMAGE_PATH_3, KEK_IMAGE_PATH_4, KEK_IMAGE_PATH_5)
