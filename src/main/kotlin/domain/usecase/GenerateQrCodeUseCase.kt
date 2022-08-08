78ebc3f54708b65535ac87dbc68740e876d00d72
Vladimir Vetrov <pdastore1@gmail.com>
2022-08-08 20:26:16 +0300

    Refactor QrCodeGenerator

--- src/main/kotlin/domain/usecase/GenerateQrCodeUseCase.kt
+++ src/main/kotlin/domain/usecase/GenerateQrCodeUseCase.kt
@@ -1,10 +1,13 @@
 package domain.usecase
 
+import com.google.zxing.EncodeHintType
 import utils.qr.QrCodeGenerator
 
-// GenerateQrCodeUseCase is used for generating image files containing qr encoded data
+// GenerateQrCodeUseCase is used for generating image files containing QR encoded data
 class GenerateQrCodeUseCase(private val qrCodeGenerator: QrCodeGenerator) {
-    suspend operator fun invoke(data: String, path: String, h: Int, w: Int) {
-        qrCodeGenerator.generateQrCode(data = data, path = path, h = h, w = w)
+    suspend operator fun invoke(data: String, filePath: String, h: Int, w: Int) {
+        // Required to support cyrillic alphabet
+        val hints = mapOf(EncodeHintType.CHARACTER_SET to "UTF-8")
+        qrCodeGenerator.generateQrCode(data = data, filePath = filePath, h = h, w = w, hints = hints)
     }
 }
--- src/main/kotlin/utils/qr/QrCodeGenerator.kt
+++ src/main/kotlin/utils/qr/QrCodeGenerator.kt
@@ -14,20 +14,25 @@ import kotlin.io.path.Path
 
 class QrCodeGenerator {
 
-    /** Generates PNG file containing a QR code from [data] and saves it into [path] */
-    suspend fun generateQrCode(data: String, path: String, charset: String = "UTF-8", h: Int, w: Int): String =
+    /** Generates PNG file containing a QR code from [data] and saves it into [filePath] */
+    suspend fun generateQrCode(
+        data: String,
+        filePath: String,
+        charset: String = "UTF-8",
+        h: Int,
+        w: Int,
+        hints: Map<EncodeHintType, String>
+        ): String =
         suspendCancellableCoroutine { continuation ->
             try {
-                // Required to support cyrillic alphabet
-                val hints = mapOf(EncodeHintType.CHARACTER_SET to "UTF-8")
                 // Encode data to QR code image 2D bit matrix
                 val matrix = MultiFormatWriter().encode(
                     String(data.toByteArray(charset(charset))), BarcodeFormat.QR_CODE, w, h, hints
                 )
                 // MultiFormatWriter is a factory class that finds the appropriate Writer subclass for the BarcodeFormat requested and encodes the barcode with the supplied contents.
                 // Create an image file in the provided path using the matrix
-                MatrixToImageWriter.writeToPath(matrix, path.substring(path.lastIndexOf('.') + 1), Path(path))
-                continuation.resume(path)
+                MatrixToImageWriter.writeToPath(matrix, filePath.substring(filePath.lastIndexOf('.') + 1), Path(filePath))
+                continuation.resume(filePath)
             } catch (e: WriterException) {
                 continuation.resumeWithException(e)
             } catch (e: IOException) {
