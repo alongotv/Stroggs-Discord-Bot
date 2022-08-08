01d457eef49758ae23292386dfda217fdacc179b
Vladimir Vetrov <pdastore1@gmail.com>
2022-08-08 17:02:11 +0300

    Add QrMessageHandler to generateMessageHandlers()

--- src/main/kotlin/domain/MessageHandlerFactory.kt
+++ src/main/kotlin/domain/MessageHandlerFactory.kt
@@ -1,10 +1,19 @@
 package domain
 
 import data.MessageCreateEventTransmitter
+import domain.message_handlers.KekMessageHandler
+import domain.message_handlers.QrMessageHandler
+import domain.usecase.GenerateQrCodeUseCase
+import domain.usecase.RemoveLocalFileUseCase
 
-class MessageHandlerFactory(private val messageCreateEventTransmitter: MessageCreateEventTransmitter) {
+class MessageHandlerFactory(
+    private val messageCreateEventTransmitter: MessageCreateEventTransmitter,
+    private val generateQrCodeUseCase: GenerateQrCodeUseCase,
+    private val removeLocalQrCodeUseCase: RemoveLocalFileUseCase
+) {
 
     suspend fun generateMessageHandlers() {
         KekMessageHandler(messageCreateEventTransmitter).setup()
+        QrMessageHandler(messageCreateEventTransmitter, generateQrCodeUseCase, removeLocalQrCodeUseCase).setup()
     }
 }
