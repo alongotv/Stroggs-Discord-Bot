1d163c458cfdf7f1c1d460092d182193a6d0442c
Vladimir Vetrov <pdastore1@gmail.com>
2022-08-24 20:39:57 +0300

    Add dagger dependency; Update main class; Add player dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+kapt {
+    generateStubs = true
+}
+
 plugins {
     kotlin("jvm") version "1.7.0"
+    kotlin("kapt") version "1.7.10"
     application
 }
 
@@ -12,39 +17,51 @@ repositories {
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
     maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://m2.dv8tion.net/releases")
+
 }
 
 dependencies {
-    val kordVersion = "0.8.0-M15"
+    val kordVersion = "0.8.0-M16"
     val zxingVersion = "3.5.0"
+    val lavaPlayerVersion = "1.3.77"
+    val daggerVersion = "2.43.2"
 
-    implementation( "org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     implementation("com.google.zxing:core:$zxingVersion")
     implementation("com.google.zxing:javase:$zxingVersion")
 
     testImplementation(kotlin("test"))
-    implementation("dev.kord:kord-core:$kordVersion")
+    implementation("dev.kord:kord-core:$kordVersion") {
+        capabilities {
+            requireCapability("dev.kord:core-voice:$kordVersion")
+        }
+    }
     implementation("dev.kord:kord-voice:$kordVersion")
+    implementation("com.sedmelluq:lavaplayer:$lavaPlayerVersion")
+
 
     implementation("org.slf4j:slf4j-simple:1.7.36")
 
+    implementation ("com.google.dagger:dagger:$daggerVersion")
+    kapt("com.google.dagger:dagger-compiler:$daggerVersion")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile>() {
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
 application {
-    mainClass.set("ApplicationKt")
+    mainClass.set("com.alongo.discordbot.MainKt")
 }
 
 tasks.withType<Jar> {
-    manifest.attributes["Main-Class"] = "ApplicationKt"
+    manifest.attributes["Main-Class"] = "com.alongo.discordbot.MainKt"
     val dependencies = configurations
         .runtimeClasspath
         .get()
