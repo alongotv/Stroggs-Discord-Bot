bd89d3a55e716a5d9ceafd4bd879a673e043e2a3
vlavet <pdastore1@gmail.com>
2022-09-11 17:45:06 +0300

    Add class CommandTypeResolver

---
+++ src/main/kotlin/com/alongo/discordbot/domain/message_handlers/CommandTypeResolver.kt
@@ -0,0 +1,13 @@
+package com.alongo.discordbot.domain.message_handlers
+
+class CommandTypeResolver {
+    private val commands = COMMAND_TYPE.values()
+
+    fun resolve(commandValue: String): COMMAND_TYPE? {
+        return commands.firstOrNull {
+            commandValue.startsWith(it.command) || (commandValue.contains(it.command) && !commandValue.startsWith(
+                '!'
+            ))
+        }
+    }
+}
