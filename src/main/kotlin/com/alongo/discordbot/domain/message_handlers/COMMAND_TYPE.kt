d3a17fedd31ae104a89d74e22dd7642dfca33802
vlavet <pdastore1@gmail.com>
2022-09-11 17:44:18 +0300

    Add enum class COMMAND_TYPE

---
+++ src/main/kotlin/com/alongo/discordbot/domain/message_handlers/COMMAND_TYPE.kt
@@ -0,0 +1,10 @@
+package com.alongo.discordbot.domain.message_handlers
+
+enum class COMMAND_TYPE(val command: String) {
+    KEK("kek"),
+    KEK_CYRILLIC("кек"),
+    QR_ENCODE("!qrencode"),
+    QR_DECODE("!qrdecode"),
+    HELP("!help"),
+    AUDIO_MESSAGE("!play")
+}
