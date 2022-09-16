9981ce4e1064cef1acca7dfa5843fb6831d97c2e
Vladimir Vetrov <pdastore1@gmail.com>
2022-09-16 21:22:50 +0300

    Register remote source with DefaultAudioPlayerManager

--- src/main/kotlin/com/alongo/discordbot/di/data/audio/LavaPlayerAudioPlayerManagerModule.kt
+++ src/main/kotlin/com/alongo/discordbot/di/data/audio/LavaPlayerAudioPlayerManagerModule.kt
@@ -1,11 +1,15 @@
 package com.alongo.discordbot.di.data.audio
 
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers
 import dagger.Module
 import dagger.Provides
 
 @Module
 class LavaPlayerAudioPlayerManagerModule {
     @Provides
-    fun provideAudioPlayerManager(): DefaultAudioPlayerManager = DefaultAudioPlayerManager()
+    fun provideAudioPlayerManager(): DefaultAudioPlayerManager = DefaultAudioPlayerManager().apply {
+        // to use YouTube, we tell LavaPlayer to use remote sources, like YouTube.
+        AudioSourceManagers.registerRemoteSources(this)
+    }
 }
