948c64c75efbae5d681da845178872bfc2192e6a
Vladimir Vetrov <pdastore1@gmail.com>
2022-09-16 22:33:53 +0300

    Handle EndOfTrackQueueException

--- src/main/kotlin/com/alongo/discordbot/data/audio/LavaPlayerClient.kt
+++ src/main/kotlin/com/alongo/discordbot/data/audio/LavaPlayerClient.kt
@@ -1,8 +1,10 @@
 package com.alongo.discordbot.data.audio
 
+import com.alongo.discordbot.domain.exceptions.EndOfTrackQueueException
 import com.alongo.discordbot.domain.message_handlers.audio.playTrack
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager
+import com.sedmelluq.discord.lavaplayer.player.event.TrackEndEvent
 import dev.kord.common.entity.Snowflake
 import javax.inject.Inject
 
@@ -16,6 +18,17 @@ class LavaPlayerClient @Inject constructor(
             players[memberVoiceChannelId] = lavaPlayerManager.createPlayer()
         }
         val player = players[memberVoiceChannelId]!!
+
+        player.addListener {
+            when (it) {
+                is TrackEndEvent -> {
+                    stopTrack(memberVoiceChannelId)
+                    // Propagate cancellation of keeping voice channel alive
+                    throw EndOfTrackQueueException()
+                }
+            }
+        }
+
         lavaPlayerManager.playTrack(query, player)
         return player
     }
