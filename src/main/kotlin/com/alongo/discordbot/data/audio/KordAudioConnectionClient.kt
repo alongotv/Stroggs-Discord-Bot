a135066d02b6a89e265d0d56e50a9dc80db587b5
vlavet <pdastore1@gmail.com>
2022-09-16 19:45:21 +0300

    Add class KordAudioConnectionClient

---
+++ src/main/kotlin/com/alongo/discordbot/data/audio/KordAudioConnectionClient.kt
@@ -0,0 +1,42 @@
+package com.alongo.discordbot.data.audio
+
+import dev.kord.common.annotation.KordVoice
+import dev.kord.common.entity.Snowflake
+import dev.kord.core.Kord
+import dev.kord.core.behavior.channel.connect
+import dev.kord.core.entity.channel.VoiceChannel
+import dev.kord.voice.AudioFrame
+import dev.kord.voice.AudioProvider
+import dev.kord.voice.VoiceConnection
+import kotlinx.coroutines.Deferred
+import javax.inject.Inject
+
+// Class for tracking current audio connections
+class KordAudioConnectionClient @Inject constructor(private val kord: Deferred<Kord>) {
+
+    // Live connection storage
+    @OptIn(KordVoice::class)
+    private val connections: MutableMap<Snowflake, VoiceConnection> = mutableMapOf()
+
+    @OptIn(KordVoice::class)
+    suspend fun connect(guildId: Snowflake, memberVoiceChannelId: Snowflake, data: ByteArray) {
+        // Discord only allows one voice connection per guild
+        // Check if there is an active voice connection in specific server
+        if (connections.contains(guildId)) {
+            connections.remove(guildId)!!.shutdown()
+        }
+        // The channel id current user is connected to
+        val voiceChannel = kord.await().getChannelOf<VoiceChannel>(id = memberVoiceChannelId)!!
+
+        val connection = voiceChannel.connect {
+            // the audio provider should provide frames of audio
+            audioProvider { AudioFrame.fromData(data) }
+        }
+        connections[guildId] = connection
+    }
+
+    @OptIn(KordVoice::class)
+    suspend fun disconnect(guildId: Snowflake) {
+        connections[guildId]?.shutdown()
+    }
+}
