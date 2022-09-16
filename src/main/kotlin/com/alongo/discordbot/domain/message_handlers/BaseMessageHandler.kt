2ce143b9f429fa2ae086440ebebe446e5a9f3945
Vladimir Vetrov <pdastore1@gmail.com>
2022-09-16 22:56:59 +0300

    Add messageHandlerScope

--- src/main/kotlin/com/alongo/discordbot/domain/message_handlers/BaseMessageHandler.kt
+++ src/main/kotlin/com/alongo/discordbot/domain/message_handlers/BaseMessageHandler.kt
@@ -1,7 +1,10 @@
 package com.alongo.discordbot.domain.message_handlers
 
 import dev.kord.core.event.message.MessageCreateEvent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
 abstract class BaseMessageHandler {
     abstract suspend fun handle(command: String, event: MessageCreateEvent)
+    protected val messageHandlerScope = CoroutineScope(Dispatchers.IO)
 }
