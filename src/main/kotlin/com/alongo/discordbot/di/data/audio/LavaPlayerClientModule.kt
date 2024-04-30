7a869e308b6dd7eaa3e6ac171797167af1e2d9f9
Vladimir Vetrov <pdastore1@gmail.com>
2024-05-01 01:20:59 +0300

    Add providePlayerStorage function to LavaPlayerClientModule

--- src/main/kotlin/com/alongo/discordbot/di/data/audio/LavaPlayerClientModule.kt
+++ src/main/kotlin/com/alongo/discordbot/di/data/audio/LavaPlayerClientModule.kt
@@ -1,6 +1,7 @@
 package com.alongo.discordbot.di.data.audio
 
 import com.alongo.discordbot.data.audio.LavaPlayerClient
+import com.alongo.discordbot.data.datasource.PlayerStorage
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager
 import dagger.Module
 import dagger.Provides
@@ -8,6 +9,11 @@ import javax.inject.Singleton
 
 @Module
 class LavaPlayerClientModule {
+    @Provides
+    @Singleton
+    fun providePlayerStorage(defaultAudioPlayerManager: DefaultAudioPlayerManager) =
+        PlayerStorage(defaultAudioPlayerManager)
+
     @Provides
     @Singleton
     fun provideLavaPlayerClient(defaultAudioPlayerManager: DefaultAudioPlayerManager) =
