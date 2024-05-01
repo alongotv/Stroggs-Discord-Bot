638d1aaaa6367c7449b3045dd7b3d3ff5c36ec5d
Vladimir Vetrov <pdastore1@gmail.com>
2024-05-01 16:14:19 +0300

    Move magic number to constants

--- src/main/kotlin/com/alongo/discordbot/Main.kt
+++ src/main/kotlin/com/alongo/discordbot/Main.kt
@@ -2,6 +2,7 @@ package com.alongo.discordbot
 
 // Application entry point object
 object Main {
+    @Suppress("MemberNameEqualsClassName") // Required to run the app
     @JvmStatic
     fun main(args: Array<String>) {
         Application().start()
