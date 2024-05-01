e4692514a054c0c8091c3ceb95dd31a63229ba04
Vladimir Vetrov <pdastore1@gmail.com>
2024-05-01 14:20:57 +0300

    Delete messages requesting pause, play and resume commands

--- src/main/kotlin/com/alongo/discordbot/domain/message_handlers/audio/ResumeAudioMessageHandler.kt
+++ src/main/kotlin/com/alongo/discordbot/domain/message_handlers/audio/ResumeAudioMessageHandler.kt
@@ -3,7 +3,9 @@ package com.alongo.discordbot.domain.message_handlers.audio
 import com.alongo.discordbot.data.audio.LavaPlayerClient
 import com.alongo.discordbot.data.datasource.PlayerStorage
 import com.alongo.discordbot.domain.message_handlers.BaseMessageHandler
+import dev.kord.core.entity.ReactionEmoji
 import dev.kord.core.event.message.MessageCreateEvent
+import kotlinx.coroutines.delay
 import javax.inject.Inject
 
 class ResumeAudioMessageHandler @Inject constructor(
@@ -15,5 +17,9 @@ class ResumeAudioMessageHandler @Inject constructor(
         val voiceChannelId = event.member?.getVoiceStateOrNull()?.channelId ?: return
         val player = playerStorage[voiceChannelId]
         lavaPlayerClient.resumeTrack(player)
+        val resumeEmoji = ReactionEmoji.Unicode("▶️")
+        event.message.addReaction(resumeEmoji)
+        delay(3000L)
+        event.message.delete()
     }
 }
